188a3fb35ac4cd1620f99ab0e6710b472cd9c524
Cavdy <cavdyofficials@gmail.com>
2020-01-21 03:49:13 +0100

    setup mongo db

--- config/db.go
+++ config/db.go
@@ -14,7 +14,7 @@ import (
 
 func Connect() {
 	// Database Config
-	clientOptions := options.Client().ApplyURI("mongodb://user:password@cluster0-shard-00-01-4j234.mongodb.net:27017/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
+	clientOptions := options.Client().ApplyURI("mongodb://user:password@host:port/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
 	client, err := mongo.NewClient(clientOptions)
 
 	//Set up a context required by mongo.Connect
